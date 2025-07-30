3d81622584f2f8904999c4a10b6e3bdc738b0178
dbco0409 <75134167+dbco0409@users.noreply.github.com>
2025-07-30 11:35:11 +0900

    update

--- project/js/index.js
+++ project/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-	let typingtxts = ["FAST", "SMART", "YOURS", "VALUE", "READY"];
-	let typingtxtsKor = ["빠르게, 원하는 시간에","똑똑하게, 합리적인 선택","당신의 방식대로","가치 있는 경험","언제든지 준비 완료"];
+	let typingtxts = ["SPEED EXPERTS", "SMART CHOICE", "PERSONAL DRIVE", "VALUE JOURNEY", "ALWAYS READY"];
+	let typingtxtsKor = ["당신이 필요한 순간, 가장 빠르게 도착합니다.<br/>위카는 신속한 예약과 출고 시스템으로 언제 어디서나 바로 이용할 수 있는 렌터카 서비스를 제공합니다.","위카는 합리적인 요금과 투명한 운영으로 사용자 중심의 편리함을 제공합니다. <br/>똑똑한 선택이 시작되는 곳, 위카.","원하는 시간, 원하는 장소, 원하는 차량까지. <br/>위카는 고객의 라이프스타일에 맞춘 유연한 서비스로, 나만의 이동 방식을 존중합니다.","단순한 이동 수단을 넘어서, <br/>여행과 일상의 가치를 더합니다. 위카는 고객 한 사람 한 사람의 경험이 <br/>더 나은 내일로 이어질 수 있도록 돕습니다.","예상치 못한 순간에도 걱정 없이. 24시간 언제든지 예약과 이용이 가능한 시스템으로, <br/>위카는 당신의 모든 여정에 대비되어 있습니다."];
 	let currentIdx = 0;
 	let typingBool = false;
 	let typingIdx = 0;
@@ -20,12 +20,7 @@ $(document).ready(function() {
 				typingIdx++;
 			} else {
 				clearInterval(tyInt);
-				$(".typing-sm").text(typingSm).fadeIn(400, function() {
-					setTimeout(function() {
-						$(".typing-sm").fadeOut(400);
-						setTimeout(startDeleting, 400); // fadeOut 후 삭제 시작
-					}, 2000); // 보여지는 시간
-				});
+				$(".typing-sm").html(typingSm).fadeIn();
 			}
 		}, 100);
 	}
